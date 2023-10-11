b2ff24cb380d80dd4d7dd5b8736349791926e115
Warren Gabrillo <wgabrillox@gmail.com>
2023-10-10 21:42:28 -0400

    Updates styles

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,12 @@ export default function Home() {
   return (
     <div className="container">
       <div className="beta">BETA</div>
-      <h3 className="header">Personal Site Coming Soon</h3>
-      <p className="footer">&#169; Warren Gabrillo X</p>
+      <div className="header">
+        <h1>Personal Site Coming Soon</h1>
+      </div>
+      <div className="footer">
+        <p>&#169; Warren Gabrillo X</p>
+      </div>
     </div>
   );
 }
