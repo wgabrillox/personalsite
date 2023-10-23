6d660875e1ae65d0393efd7f75612709ab1a203e
Warren Gabrillo <wgabrillox@gmail.com>
2023-10-23 18:31:14 -0400

    Base for about page

--- components/main/header.jsx
+++ components/main/header.jsx
@@ -1,17 +1,25 @@
 import styles from "./header.module.css";
+import Link from "next/link";
 
-export default function Header() {
+export default function Header({ active }) {
   return (
     <div className={styles.headerWrapper}>
-      <div className={styles.nameLogo}>
-        <div className={styles.firstName}>Warren</div>
-        <div className={styles.lastName}>Gabrillo</div>
-      </div>
+      <Link href={"/"}>
+        <div className={styles.nameLogo}>
+          <div className={styles.firstName}>Warren</div>
+          <div className={styles.lastName}>Gabrillo</div>
+        </div>
+      </Link>
       <div className={styles.spacer} />
       <div className={styles.links}>
         {/* TODO: Add underline animation on hover */}
-        <div className={styles.link}>About</div>
-        <div className={styles.link}>Project</div>
+        <div
+          className={`${styles.link} ${active == "about" ? styles.active : ""}`}
+        >
+          <Link href={"/about"}>About</Link>
+        </div>
+        {/* <Link href={`/posts/${id}`}>{title}</Link> */}
+        <div className={styles.link}>Projects</div>
         {/* TODO: Add button animation on hover/on click */}
         <div className={styles.contact}>Contact</div>
       </div>
