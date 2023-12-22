897d7d438a86102e3f613a1130601b4e29eb8e88
Warren Gabrillo <wgabrillox@gmail.com>
2023-12-22 17:49:00 -0500

    Connects experiences section to mongodb

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,26 @@ import Header from "../components/main/header";
 import Bio from "../components/main/bio";
 import Experiences from "../components/main/experiences";
 import Skills from "../components/main/skills";
-import * as d3 from "d3";
-export default function Home() {
+import { connectToDatabase } from "../lib/mongodb";
+
+export default function Home({ experiences }) {
   return (
     <div>
       {/* <Beta /> */}
       <Header />
       <Bio />
-      <Experiences />
+      <Experiences experiences={experiences} />
       <Skills />
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  let { db } = await connectToDatabase();
+
+  const experiences = await db.collection("Experiences").find().toArray();
+
+  return {
+    props: { experiences: JSON.parse(JSON.stringify(experiences)) },
+  };
 }
