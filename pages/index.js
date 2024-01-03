7905ffc5599cf2e8168480239385483461b094bf
Warren Gabrillo <wgabrillox@gmail.com>
2024-01-03 12:57:16 -0500

    Adds skills to mongodb

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ import Experiences from "../components/main/experiences";
 import Skills from "../components/main/skills";
 import { connectToDatabase } from "../lib/mongodb";
 
-export default function Home({ experiences }) {
+export default function Home({ experiences, skills }) {
   return (
     <div>
       {/* <Beta /> */}
       <Header />
       <Bio />
       <Experiences experiences={experiences} />
-      <Skills />
+      <Skills skills={skills} />
     </div>
   );
 }
@@ -22,8 +22,12 @@ export async function getServerSideProps() {
   let { db } = await connectToDatabase();
 
   const experiences = await db.collection("Experiences").find().toArray();
+  const skills = await db.collection("Skills").find().toArray();
 
   return {
-    props: { experiences: JSON.parse(JSON.stringify(experiences)) },
+    props: {
+      experiences: JSON.parse(JSON.stringify(experiences)),
+      skills: JSON.parse(JSON.stringify(skills)),
+    },
   };
 }
