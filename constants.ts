fe9101a5fd97ec596dead28f486999e9313b6267
Warren Gabrillo <wgabrillox@gmail.com>
2024-07-12 16:27:50 -0400

    adds constants file and next config

---
+++ constants.ts
@@ -0,0 +1,20 @@
+interface ImagesList {
+  [key: string]: string;
+}
+
+export const IMAGES: ImagesList = {
+  weddingPic:
+    "https://res.cloudinary.com/dhtbrbpni/image/upload/v1720804584/Personal%20Site/wedding_emudmo.jpg",
+  mantou:
+    "https://res.cloudinary.com/dhtbrbpni/image/upload/v1720804582/Personal%20Site/mantou_pdyy2a.jpg",
+  alien:
+    "https://res.cloudinary.com/dhtbrbpni/image/upload/v1720804581/Personal%20Site/alien_wruq0r.jpg",
+  figma:
+    "https://res.cloudinary.com/dhtbrbpni/image/upload/v1720804502/Personal%20Site/figma-icon_mjsucj.png",
+  github:
+    "https://res.cloudinary.com/dhtbrbpni/image/upload/v1720804503/Personal%20Site/github-logo_umimfj.png",
+  trello:
+    "https://res.cloudinary.com/dhtbrbpni/image/upload/v1720804505/Personal%20Site/trello-logo_xngwui.jpg",
+  lorcanaGuess:
+    "https://res.cloudinary.com/dhtbrbpni/image/upload/v1720804505/Personal%20Site/lorcana-guess_phqwfs.png",
+};
